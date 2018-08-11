72b6d03f6873664b16c01ff5e9f24faf1d4ab754
Ferdinand Aton <featon415@gmail.com>
2018-08-10 19:44:32 -0500

    Base for Prototype1

---
+++ LudumDare42 - 2D TopDown Constricting Walls/Assets/Scripts/BlackholeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackholeController : MonoBehaviour {
+
+    CircleCollider2D startMarker;
+    bool cullStart;
+    float holeScale;
+    public float currentScale;
+    public float maxScale;
+    public float scaleRate;
+
+    public GameObject floorHole;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        startMarker = transform.GetComponent<CircleCollider2D>();
+        currentScale = transform.localScale.x;
+        maxScale = 1000f;
+        scaleRate = 0.1f;
+
+	}
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!cullStart)
+        {
+            Debug.Log("Start!");
+            Destroy(startMarker);
+            Destroy(floorHole);
+            cullStart = true;
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        if (cullStart)
+        {
+            holeScale = Mathf.MoveTowards(currentScale, maxScale, scaleRate * Time.deltaTime);
+            currentScale = holeScale;
+            transform.localScale = new Vector3(holeScale, holeScale, 1);
+
+        }
+
+	}
+}
---
+++ LudumDare42 - 2D TopDown Constricting Walls/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float movementX;
+    float movementY;
+    Vector2 deltaMove;
+    public int speed = 10;
+
+    Rigidbody2D rB;
+
+	// Use this for initialization
+	void Start () {
+
+        rB = transform.GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        movementX = Input.GetAxis("Horizontal");
+        movementY = Input.GetAxis("Vertical");
+
+        deltaMove = new Vector2(movementX *speed , movementY * speed);
+
+	}
+
+    void FixedUpdate()
+    {
+
+        rB.MovePosition(rB.position + deltaMove *Time.fixedDeltaTime);
+
+    }
+
+}
