0bd9d17deef6fa41835c6df60ac0e306c17a4ad5
featon <featon415@gmail.com>
2018-08-11 21:06:50 -0500

    Add dash

--- LudumDare42 - 2D TopDown Constricting Walls/Assets/Scripts/PlayerController.cs
+++ LudumDare42 - 2D TopDown Constricting Walls/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,14 @@ public class PlayerController : MonoBehaviour {
     float movementX;
     float movementY;
     Vector2 deltaMove;
-    public int speed = 10;
+    int speed = 3;
+
+    DashState dashState = DashState.Ready;
+    float dashTimer = 0f;
+    float maxDashTimer = 0.1f;
+    float cooldownTimer = 0.1f;
+    float cooldownTimerCopy;
+    int previousSpeed = 0;
 
     Rigidbody2D rB;
 
@@ -21,25 +28,68 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        movementX = Input.GetAxis("Horizontal");
-        movementY = Input.GetAxis("Vertical");
+        movementX = Input.GetAxisRaw("Horizontal");
+        movementY = Input.GetAxisRaw("Vertical");
 
-        deltaMove = new Vector2(movementX *speed , movementY * speed);
+        switch (dashState)
+        {
+            case DashState.Ready:
+                if (Input.GetButtonDown("Dash"))
+                {
+                    previousSpeed = speed;
+                    speed = 20;
+                    dashState = DashState.Dashing;
+                }
+                break;
+            case DashState.Dashing:
+                dashTimer += Time.deltaTime;
+                if (dashTimer >= maxDashTimer)
+                {
+                    dashTimer = maxDashTimer;
+                    cooldownTimerCopy = cooldownTimer;
+                    dashState = DashState.Cooldown;
+                }
+                break;
+            case DashState.Cooldown:
+                speed = previousSpeed;
+                cooldownTimer -= Time.deltaTime;
+                if (cooldownTimer <= 0)
+                {
+                    dashTimer = 0;
+                    cooldownTimer = cooldownTimerCopy;
+                    dashState = DashState.Ready;
+                }
+                break;
+        }
+
+        // TODO: have speed of diagonal movement reduced to the cardinal movement
+        deltaMove = new Vector2(movementX * speed , movementY * speed);
 
 	}
 
     void FixedUpdate()
     {
 
-        rB.MovePosition(rB.position + deltaMove *Time.fixedDeltaTime);
+        rB.MovePosition(rB.position + deltaMove * Time.fixedDeltaTime);
 
+        // Get mouse position from perspective of screen
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // TODO: understand this
         Quaternion rotation = Quaternion.LookRotation(transform.position - mousePos, Vector3.forward);
 
         transform.rotation = rotation;
+        // Have rotation only on the z axis
         transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
         rB.angularVelocity = 0;
+        
 
     }
 
+}
+
+enum DashState
+{
+    Ready,
+    Dashing,
+    Cooldown
 }
